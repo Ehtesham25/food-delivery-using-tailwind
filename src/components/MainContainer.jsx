a3a009f81e8b1e4770f6ba1540ef7f126d967b57
ehtesham25 <ehteshamulhaqkhan25@gmail.com>
2022-09-08 23:25:49 +0500

    main container

---
+++ src/components/MainContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Home from './Home'
+
+const MainContainer = () => {
+    return (
+        <div className='w-full h-auto flex flex-col items-center justify-between'>
+            <Home />
+        </div>
+    )
+}
+
+export default MainContainer
