f9e4213d226223c8fa52fb46b032b77033e67eae
ehtesham25 <ehteshamulhaqkhan25@gmail.com>
2022-09-08 23:26:47 +0500

    index file

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Header } from "./Header"
+export { default as MainContainer } from "./MainContainer"
+export { default as Home } from "./Home"
+export { default as CreateItem } from "./CreateItem"
+export { default as Loading } from "./Loading"
