c13483328d0b97e4e1c56b693d0567825a826d52
ehtesham25 <ehteshamulhaqkhan25@gmail.com>
2022-09-19 12:33:51 +0500

    dishes

--- src/components/Dishes.jsx
+++ src/components/Dishes.jsx
@@ -17,7 +17,7 @@ const Dishes = () => {
             <Text text="Our hot and spicy dishes." />
             <div className='flex items-center gap-5 justify-center my-7 py-8 overflow-x-scroll scrollbar-none'>
                 {categories && categories.map((item, index) => (
-                    <div id={index} onClick={() => handleSelect(item.urlParamName)} className={` ${item.urlParamName === selectedItem ? "bg-cartNumBg" : "bg-card"}  group flex h-28 min-win-[94px] w-24 flex-col items-center justify-center  rounded-lg  drop-shadow-lg hover:bg-cartNumBg  cursor-pointer transition-all ease-in-out duration-200`}>
+                    <div key={index} onClick={() => handleSelect(item.urlParamName)} className={` ${item.urlParamName === selectedItem ? "bg-cartNumBg" : "bg-card"}  group flex h-28 min-win-[94px] w-24 flex-col items-center justify-center  rounded-lg  drop-shadow-lg hover:bg-cartNumBg  cursor-pointer transition-all ease-in-out duration-200`}>
                         <div className={`${item.urlParamName === selectedItem ? "bg-card" : "bg-cartNumBg"} w-10 h-10  rounded-full flex justify-center items-center group-hover:bg-white `}>
                             <MdFastfood className={` ${item.urlParamName !== selectedItem ? "text-card" : "text-headingColor"}  group-hover:text-textColor text-lg`} />
                         </div>
@@ -26,6 +26,7 @@ const Dishes = () => {
                 ))}
             </div>
             <div className='w-full flex items-center '>
+
                 <RowContainer flag={false} />
             </div>
         </section>
