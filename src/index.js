404a5379f324437dc90e8add3d10dbe59ba149af
ehtesham25 <ehteshamulhaqkhan25@gmail.com>
2022-09-19 12:34:14 +0500

    index

--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { StateProvider } from './context/StateProvider';
-import { initialState } from "./context/initialState"
-import reducer from "./context/reducer"
 import { BrowserRouter } from 'react-router-dom';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <StateProvider initialState={initialState} reducer={reducer}  >
+  <StateProvider >
     <BrowserRouter>
       <React.StrictMode>
         <App />
