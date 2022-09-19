99beb3468f4c4e84042b1fc40a0e0984c1779de6
ehtesham25 <ehteshamulhaqkhan25@gmail.com>
2022-09-19 12:32:41 +0500

    reducer

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -1,13 +1,23 @@
 import { userTypes } from "./actionTypes";
+import { INITIAL_STATE } from "./StateProvider";
 
-const reducer = (state, action) => {
+
+const reducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case userTypes.SET_USER:
             localStorage.setItem("user", JSON.stringify(action.payload))
             return { ...state, user: action.payload }
+        case userTypes.SET_CART_SHOW:
+            return { ...state, isCartShow: !state.isCartShow }
+        case userTypes.SET_ADD_TO_CART:
+            return { ...state, cartItems: [...state.cartItems, action.payload] }
+        case userTypes.SET_QTY:
+            return { ...state, cartItems: action.payload }
+        case userTypes.SET_CLEAR_CART:
+            return { ...state, cartItems: [] }
         default:
             return state;
     }
-}
+};
 
 export default reducer
