76407abc19599617d0b2d43de2529a3d29d07ed1
Ehtesham Khan <72070084+Ehtesham25@users.noreply.github.com>
2023-08-12 16:17:27 +0500

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { homeData } from '../utils/data'
+import { homeData } from '../utils/data';
 import delivery from "../assets/delivery.png"
 import heroBg from "../assets/heroBg.png"
 
